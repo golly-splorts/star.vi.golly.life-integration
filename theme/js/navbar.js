366dc8092f23b7609515cc6383f32588affaa9c0
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2025-08-22 17:54:58 -0700

    Automatic deploy of integration at 2025-08-23-00-54-58

--- theme/js/navbar.js
+++ theme/js/navbar.js
@@ -31,6 +31,7 @@
           // Populate season drop-down
           var sdropElem = document.createElement('a');
           sdropElem.classList.add('dropdown-item');
+          //sdropElem.setAttribute('href', baseUIUrl + '/season.html?season=' + season0);
           sdropElem.setAttribute('href', baseUIUrl + '/season.html?which_season=' + sp1);
           sdropElem.innerHTML = 'Season ' + sp1;
           navbarSeasonDropdown.appendChild(sdropElem);
@@ -38,6 +39,7 @@
           // Populate postseason drop-down
           var pdropElem = document.createElement('a');
           pdropElem.classList.add('dropdown-item');
+          //pdropElem.setAttribute('href', baseUIUrl + '/postseason.html?season=' + season0);
           pdropElem.setAttribute('href', baseUIUrl + '/postseason.html?which_season=' + sp1);
           pdropElem.innerHTML = 'Season ' + sp1;
           navbarPostseasonDropdown.appendChild(pdropElem);
