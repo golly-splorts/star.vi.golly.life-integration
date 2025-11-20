7ceab969a3585a710f9b9319126eeaa31941ee09
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2025-11-20 13:34:09 -0800

    Automatic deploy of integration at 2025-11-20-21-34-09

--- league.js
+++ league.js
@@ -614,7 +614,15 @@
               tdPct.textContent = pct.toFixed(2);
               tr.appendChild(tdPct);
 
-              // Col 5: GB
+              // Col 5: Games Left
+              const dps = 49;
+              var tdGl = document.createElement('td');
+              tdGl.classList.add('text-center', 'games-left-col');
+              const games_played = our_wins + our_losses;
+              tdGl.textContent = dps - games_played;
+              tr.appendChild(tdGl);
+
+              // Col 6: GB
               var tdGb = document.createElement('td');
               tdGb.classList.add('text-center'); // Align right
               if (iS === 0) {
@@ -627,12 +635,6 @@
               }
               tr.appendChild(tdGb);
 
-              // Col 6: GL
-              var tdGl = document.createElement('td');
-              tdGl.classList.add('text-center', 'gl-col'); // Align right
-              tdGl.textContent = dps - (our_wins + our_losses);
-              tr.appendChild(tdGl);
-
               // Col 7: Elim #
               var tdElim = document.createElement('td');
               tdElim.classList.add('text-center'); // Align right
@@ -644,7 +646,7 @@
               }
               tr.appendChild(tdElim);
 
-              // Col 7: WC Elim #
+              // Col 8: WC Elim #
               var tdWcElim = document.createElement('td');
               tdWcElim.classList.add('text-center'); // Align right
               if (iS === 0) {
